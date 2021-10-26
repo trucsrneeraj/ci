b5da7b87fd80d8294d2ae5f0c98527ac93409170
Neerajkumar <neeraj.prajapati@trucsr.in>
2021-10-26 17:34:11 +0530

    file change

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -24,7 +24,7 @@ class Welcome extends CI_Controller {
 		// $this->load->model('select');  
 		// $data['h']=$this->select->select();  
 		// $this->load->view('welcome_message', $data);  
-		//$this->load->view('welcome_message');
+		$this->load->view('welcome_message');
 		
 		
 
